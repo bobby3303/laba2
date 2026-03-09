44c9ea5703a3c36bed8338970c4d052449a05402
Vladislav <kaigorodovvlad599@gmail.com>
2026-03-09 20:56:23 +0500

    lab 2

---
+++ task21/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+
+let rec input () =
+    printfn "Введите количество элементов списка:"
+    match Int32.TryParse(Console.ReadLine()) with
+    | true, n when n >= 1 -> n
+    | _ -> 
+        eprintfn "Ошибка: введите целое положительное число!"
+        input ()
+
+let rec BuildList cnt =
+    if cnt <= 0 then 
+        []
+    else 
+        printf "Введите число (1-9): "
+        match Int32.TryParse(Console.ReadLine()) with
+        | true, value when value >= 1 && value <= 9 ->
+            value :: BuildList (cnt - 1)
+        | _ ->
+            eprintfn "Ошибка: введите целое число от 1 до 9"
+            BuildList cnt
+
+let rec toBinary n =
+    if n = 0 then 
+        "0"
+    elif n = 1 then 
+        "1"
+    else 
+        (toBinary (n / 2)) + (string (n % 2))
+
+[<EntryPoint>]
+let main args = 
+    let cnt = input()
+    let inList = BuildList cnt
+
+    let BinList = inList |> List.map toBinary
+
+    printfn "\nИсходные числа: %A" inList
+    printfn "В двоичной системе: %A" BinList
+
+    0
---
+++ task22/task22.fs
@@ -0,0 +1,54 @@
+open System
+
+let rec input () =
+    printfn "Введите количество элементов списка:"
+    match Int32.TryParse(Console.ReadLine()) with
+    | true, n when n >= 1 -> n
+    | _ -> 
+        eprintfn "Ошибка: введите целое положительное число!"
+        input ()
+
+let rec BuildList cnt =
+    if cnt <= 0 then 
+        []
+    else 
+        printf "Введите число (0-9): "
+        match Int32.TryParse(Console.ReadLine()) with
+        | true, value when value >= 0 && value <= 9 ->
+            value :: BuildList (cnt - 1)
+        | _ ->
+            eprintfn "Ошибка: введите целое число от 0 до 9"
+            BuildList cnt
+
+let digitToWord n =
+    match n with
+    | 0 -> "ноль"
+    | 1 -> "один"
+    | 2 -> "два"
+    | 3 -> "три"
+    | 4 -> "четыре"
+    | 5 -> "пять"
+    | 6 -> "шесть"
+    | 7 -> "семь"
+    | 8 -> "восемь"
+    | 9 -> "девять"
+    | _ -> "неизвестно"
+
+let folder acc n =
+    let word = digitToWord n
+    if acc = "" then 
+        word
+    else 
+        acc + "; " + word
+
+[<EntryPoint>]
+let main args = 
+    let cnt = input()
+    let inList = BuildList cnt
+
+    let result = List.fold folder "" inList
+
+    printfn "\nИсходные числа: %A" inList
+    printfn "Результат: «%s»" result
+
+    0
